255258701f07ec8c0eabc9968482a1b1f0efdf71
Max <mlmason11@gmail.com>
2023-07-11 10:16:21 -0400

    Submitting functions lab pt2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(task = 'go to the office') {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function(adj = 'special') {
+        return `You are ${flair}${adj}${flair}!`;
+    };
+}
